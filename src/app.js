59c3fbcf1137e36ac09880c42cb686d314fa85de
breno.breves <brenocsb@dcc.ufrj.br>
2023-07-09 21:17:23 -0300

    Post de transações implementado

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import Joi from "joi";
 import bcrypt from 'bcrypt';
 import { v4 as uuid } from "uuid";
+import dayjs from "dayjs";
 
 const app = express();
 app.use(cors());
@@ -73,6 +74,53 @@ app.post("/sign-in", async(req,res) => {
         return res.status(500).send(err.message)
     }    
 })
+
+//Post transação
+
+app.post("/trans/:tipo", async(req,res) => {
+    let token = req.headers.token
+    if(!token) return res.status(401).send("Requisição sem token de validação")
+    token = token.replace("Bearer ","")
+    const tipo = req.params.tipo
+    if(tipo !== "saída" && tipo !== "entrada") return res.status(422).send("Tipo de operação não localizada/permitida")
+    const schemaTrans = Joi.object({
+        valor: Joi.number().positive().required(),
+        desc: Joi.string().required()
+    })
+
+    let {valor , desc} = req.body
+    try {
+        const sessao = await db.collection("sessoes").findOne({token}) 
+        if(!sessao) return res.status(401).send("Token inválido ou inativo")
+        const validation = schemaTrans.validate(req.body, {abortEarly: false});
+        if(validation.error){
+            const errors = validation.error.details.map(detail => detail.message);
+            return res.status(422).send(errors); 
+        }
+        const valorteste = Math.floor(valor*100)/100
+        if(valor !== valorteste){
+            return res.status(422).send("Valor com mais de 2 casas decimais")
+        }
+        const transObj = {
+            email: sessao.email,
+            tipo,
+            valor,
+            desc,
+            data: dayjs().format("DD/MM")
+        }
+        await db.collection("trans").insertOne(transObj)
+        if(tipo === "saída"){
+            await db.collection("users").updateOne({email: sessao.email},{$inc:{saldo: -valor}})
+            return res.status(201).send("Transação aceita, saldo atualizado")
+        }else{
+            await db.collection("users").updateOne({email: sessao.email},{$inc:{saldo: valor}})
+            return res.status(201).send("Transação aceita, saldo atualizado")
+        }
+
+    } catch (err) {
+        return res.status(500).send(err.message)
+    }
+    })
 
 const port = process.env.PORT || 5000;
 app.listen(port , () => console.log(`App rodando na porta ${port}`));    
